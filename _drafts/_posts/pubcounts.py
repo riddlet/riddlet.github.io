d751dc94cb96a030b25fd7c1672c276c3a443717
Travis Riddle <riddlet@gmail.com>
2014-12-03 17:31:01 -0500

    Some slight changes to the plotting

--- _drafts/_posts/pubcounts.py
+++ _drafts/_posts/pubcounts.py
@@ -5,24 +5,39 @@ Spyder Editor
 This is a temporary script file.
 """
 
+#
+# First, I import the libraries
 import pandas as pd
 import numpy as np
 import re
 from ggplot import *
 import glob
+import os
 
-files = glob.glob('/Users/triddle/Documents/riddlet.github.io/_drafts/data/Scopus-*.csv')
+#
+# Next, I extract the files, and add what year they came from
+pathdir = os.chdir('..')
+files = glob.glob('data/Scopus-*.csv')
 data = pd.DataFrame()
 for f in files:
     yearfile = pd.read_csv(f, skiprows=7)
     year = re.search('[1-2][0-9][0-9][0-9]', f)
     yearfile['Year'] = year.group(0)
     data = pd.concat([data, yearfile])
-    
+   
+#
+# Rename the columns for clarity
 data.rename(columns={'SOURCE TITLE':'Source Title', 'Unnamed: 1':'Papers'}, 
             inplace=True)
 
-plot = ggplot(data, aes(x='Year', y='Papers', color='Source Title')) +\
-    geom_line(alpha=0.5)
+#
+# Remember, we're explaining this weird jump:
+papersbyyear = data.groupby('Year')['Papers'].sum()
+papersbyyear.plot()
 
-fig = plot.draw()
+#
+# For the next set of tricks, it would be good to see if there were any jumps 
+# in the number of journals for each year.
+titlesbyyear = data.groupby('Year')['Source Title'].nunique
+
+yeargroups = data.groupby('Year')
