0f891a9082035ca4fc06ef9d670b1ade546d0d8d
Travis Riddle <riddlet@gmail.com>
2015-04-08 13:06:16 -0400

    list of urls

--- _drafts/_posts/astrology.py
+++ _drafts/_posts/astrology.py
@@ -3,11 +3,21 @@ import urllib2
 import datetime
 import pandas as pd
 
+baseurl = 'http://nypost.com/horoscope/'
+signs = ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 
+         'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces']
+
 start = pd.datetime(2013, 12, 01)
 end = datetime.datetime.today()
 rng = pd.date_range(start, end)
 for day in rng:
-	datetime.datetime.strptime(day, '%Y-%M-%d').strftime('%M-%d-%Y')
+    for sign in signs:
+        url = baseurl + sign + '-' + day.strftime('%m-%d-%Y') + '/'
+        content = urllib2.urlopen(url).read()
+        soup = BeautifulSoup(content)
+        soup = soup.find('div', 'entry-content')
+        soup = soup.find('p').string
+
 
 
 url = 'http://nypost.com/horoscope/sagittarius-01-24-2014/'
